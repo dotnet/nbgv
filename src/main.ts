cc05b188015580d9716fad6d6ced8a141ad999be
Cameron Taggart <cameron@Camerons-MacBook-Pro.local>
2019-08-18 15:27:06 +0800

    exec dotnet tool install

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
 import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+import * as fs from 'fs';
 
 async function run() {
   try {
     const myInput = core.getInput('myInput');
     core.debug(`Hello ${myInput}`);
+    core.warning(`warning a Hello ${myInput}`)
+
+    // https://github.com/actions/toolkit/tree/master/packages/exec
+    await exec.exec('dotnet', ['tool', 'install', '-g', 'nbgv']);
+
+    
+    // core.addPath('pathToTool');
+
+    let json = fs.createWriteStream('nbgv.json');
+    await exec.exec('nbgv', ['get-version', '-f', 'json'], {
+      outStream: json
+    });
+
   } catch (error) {
     core.setFailed(error.message);
   }
